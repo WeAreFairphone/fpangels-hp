f7b32d997b8bb22c2c8e46b0f5b912e709de1d10
ff2u <47901109+ff2u@users.noreply.github.com>
2023-02-04 21:41:50 +0100

    Update build.py
    Fix twemoji script url, see https://github.com/twitter/twemoji as MaxCDN is down.

--- build.py
+++ build.py
@@ -11,7 +11,7 @@ if response.status_code == requests.codes.ok:
 
     with doc.head:
         link(rel='stylesheet', href='style.css')
-        script(type='text/javascript', src='https://twemoji.maxcdn.com/v/latest/twemoji.min.js', crossorigin='anonymous')
+        script(type='text/javascript', src='https://unpkg.com/twemoji@latest/dist/twemoji.min.js', crossorigin='anonymous')
         script(type='text/javascript', src='script.js')
         meta(name='viewport', content='width=device-width')
 
